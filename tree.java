c86ad17f77489dad54c6901940fc3b18d92ea878
ChristopherJohn <southpaw1224@outlook.com>
2022-06-09 10:25:38 -0400

    first commit

---
+++ tree.java
@@ -0,0 +1,12 @@
+public class Tree {
+    public static void main(String[] args) {
+      //Going to be printing a tree
+      System.out.println("Hey there, I'm CJ and I'm coding in Java!");
+      System.out.println("Ready get my hands dirty!");
+      System.out.println("  *  ");
+      System.out.println(" *** ");
+      System.out.println(" *** ");
+      System.out.println("  *  ");
+      System.out.println("  *  ");   
+    }
+  }
